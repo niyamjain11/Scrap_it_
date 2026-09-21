2b2d37ed273e134fb8ff7c82eaf7c22bb7c85788
Niyam Jain <niyamj1102@gmail.com>
2026-09-21 19:15:22 +0530

    refactor(constants): introduce centralized platform role and language constants

---
+++ src/utils/constants.js
@@ -0,0 +1,30 @@
+/**
+ * Shared platform constants and role definitions for SCRAPIT.
+ */
+
+export const PLATFORM_ROLES = {
+  CITIZEN: 'citizen',
+  KABADIWALA: 'kabadiwala',
+  RECYCLER: 'recycler',
+};
+
+export const SUPPORTED_LANGUAGES = {
+  EN: 'en',
+  HI: 'hi',
+  MR: 'mr',
+};
+
+export const ORDER_STATUS = {
+  REQUESTED: 'Requested',
+  ACCEPTED: 'Accepted',
+  ASSIGNED: 'Executive Assigned',
+  WEIGHED: 'Weighed & Paid',
+  COMPLETED: 'Completed',
+  CANCELLED: 'Cancelled',
+};
+
+export const VEHICLE_TYPES = {
+  BICYCLE: 'Bicycle / Manual Cart',
+  E_LOADER: '3-Wheeler E-Loader',
+  MINI_TRUCK: 'Tata Ace / Mini-Truck',
+};
